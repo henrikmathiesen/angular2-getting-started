e2df61fda49c30d635074fd02632ba48b0fff755
Henrik <henrik_mathiesen@hotmail.com>
2016-07-12 15:14:58 +0200

    -configs configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
+var isDebug = process.env.NODE_ENV !== "production";
 var webpack = require('webpack');
 var path = require('path');
+var CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
 
@@ -8,6 +10,21 @@ module.exports = {
         path: path.resolve(__dirname, 'app/js/bld'),
         filename: 'app.bundle.js'
     },
+    devtool: isDebug ? "sourcemap" : null,
+
+    plugins: isDebug ? [] : [
+        new webpack.DefinePlugin({ 'process.env.NODE_ENV': '"production"' }),
+        
+        // clean map files when build for production
+        new CleanWebpackPlugin(['app/js/bld'], {
+            root: path.resolve(__dirname),
+            verbose: true,
+            dry: false
+        }),
+        
+        new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false })
+    ],
+
     module: {
         loaders: [
             {
