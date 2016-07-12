cc389d3ad237926c829ab61a3c00673142a3345c
Henrik <henrik_mathiesen@hotmail.com>
2016-07-12 12:57:17 +0200

    -configure

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+
+    entry: path.resolve(__dirname, 'app/js/src/main.ts'),
+    output: {
+        path: path.resolve(__dirname, 'app/js/bld'),
+        filename: 'app.bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.ts$/,
+                loader: 'ts-loader'
+            }
+        ]
+    },
+    resolve: {
+        extensions: [
+            '',
+            '.js',
+            '.ts'
+        ]
+    }
+
+};
