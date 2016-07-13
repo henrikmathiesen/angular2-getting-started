bb0c0da7e5ab530e7cdfd36de4ff3ff8c2f6e3a5
Henrik <henrik_mathiesen@hotmail.com>
2016-07-13 14:13:01 +0200

    -product list added

--- app/js/app.component.ts
+++ app/js/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 
+import { ProductListComponent } from './products/product-list.component';
+
 @Component({
     selector: 'gs-app',
     template: `
         <div>
             <h2>{{ pageTitle }}</h2>
+            <gs-products></gs-products>
         </div>
-    `
+    `,
+    directives: [ProductListComponent]
 })
 export class AppComponent {
-    pageTitle: string = "My Angular 2 App!";
+    pageTitle: string = "My Angular 2 App";
 }
---
+++ app/js/products/product-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'gs-products',
+    templateUrl: 'app/js/products/product-list.component.html'
+})
+export class ProductListComponent {
+
+}
