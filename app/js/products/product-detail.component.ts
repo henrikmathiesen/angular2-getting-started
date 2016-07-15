e885ba0a6f019664486f0efaa2bbf523ddee66d8
Henrik <henrik_mathiesen@hotmail.com>
2016-07-16 01:27:34 +0200

    -get route params

--- app/js/products/product-detail.component.ts
+++ app/js/products/product-detail.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 declare var require: any;
 
 @Component({
     template: require('./product-detail.component.html')
 })
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnInit {
     pageTitle = "Product detail";
+
+    constructor(private _route: ActivatedRoute) {
+
+    }
+
+    ngOnInit(): void {
+        this._route.params
+            .subscribe((params) => {
+                console.log(params["id"]);
+            })
+    }
 }
