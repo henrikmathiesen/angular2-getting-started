ab17535a357502137b3f5dd74cacba4ffbe3433c
Henrik <henrik_mathiesen@hotmail.com>
2016-07-16 21:30:36 +0200

    -testing some loops

--- app/js/products/product-list.component.ts
+++ app/js/products/product-list.component.ts
@@ -23,6 +23,23 @@ export class ProductListComponent implements OnInit {
     products: IProduct[];
     errorMessage: string;
 
+    testArray: any[] = [
+        {
+            id: 1,
+            name: "Adam"
+        },
+        {
+            id: 2,
+            name: "Bertil"
+        },
+        {
+            id: 3,
+            name: "Ceasar"
+        }
+    ];
+
+    forOfCounter: number = 0;
+
     constructor(private _productService: ProductService, private _router: Router) {
 
     }
@@ -33,6 +50,35 @@ export class ProductListComponent implements OnInit {
             (products) => this.products = products,
             (error) => this.errorMessage = error
             );
+
+        //
+        // Three kind of loops
+
+        for (var index = 0; index < this.testArray.length; index++) {
+            console.log("for loop: " + this.testArray[index].name);
+
+            if (index === 1) {
+                break;  // We can break in good old for loops
+            }
+        }
+
+        this.testArray.forEach((val, index) => {
+            console.log("forEach " + val.name);
+
+            // We can not break or continue inside forEach loops
+        });
+
+        for (let val of this.testArray) {
+            console.log("for of " + val.name);
+
+            // We can break and continue in this loop, but cant get to index without an external counter
+
+            if (this.forOfCounter === 1) {
+                break;
+            }
+
+            this.forOfCounter++;
+        }
     }
 
     toggleImage(): void {
