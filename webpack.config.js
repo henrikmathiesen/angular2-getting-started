f4a1ccc4bffc4e78eadd6e5ec1fa626f2b25c319
Henrik <henrik_mathiesen@hotmail.com>
2016-07-21 20:45:23 +0200

    -tried copy html file from app to bld folder -made comments about not going that route

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,19 @@ var webpack = require('webpack');
 var path = require('path');
 var CleanWebpackPlugin = require('clean-webpack-plugin');
 var autoprefixer = require('autoprefixer');
+//var CopyWebpackPlugin = require('copy-webpack-plugin');
+
+
+// I had an idea of placing index.html in app folder and copy it to build folder on build
+// And then serving bld folder with http-server bld -p 1337, but that messed up the api calls to api folder
+// (since it was not served) So we dont do that
+// var envSharedplugins = {
+//     copyHtml: new CopyWebpackPlugin([
+//         {
+//             from: 'app/index.html'
+//         }
+//     ])
+// }
 
 module.exports = {
 
@@ -14,36 +27,36 @@ module.exports = {
     devtool: isDebug ? "sourcemap" : null,
 
     plugins: isDebug ? [] : [
-        new webpack.DefinePlugin({ 'process.env.NODE_ENV': '"production"' }),
+            new webpack.DefinePlugin({ 'process.env.NODE_ENV': '"production"' }),
 
-        // clean map files when build for production
-        new CleanWebpackPlugin(['bld/*.map'], {
-            root: path.resolve(__dirname),
-            verbose: true,
-            dry: false
-        }),
+            // clean map files when build for production
+            new CleanWebpackPlugin(['bld/*.map'], {
+                root: path.resolve(__dirname),
+                verbose: true,
+                dry: false
+            }),
 
-        // sending in -p to webpack (see package.json), does some minification, but this does more
-        new webpack.optimize.UglifyJsPlugin({
-            sourcemap: false,
-            compress: {
-                sequences: true,
-                dead_code: true,
-                conditionals: true,
-                booleans: true,
-                unused: true,
-                if_return: true,
-                join_vars: true,
-                drop_console: true
-            },
-            mangle: {
-                except: ['$super', '$', 'exports', 'require']
-            },
-            output: {
-                comments: false
-            }
-        })
-    ],
+            // sending in -p to webpack (see package.json), does some minification, but this does more
+            new webpack.optimize.UglifyJsPlugin({
+                sourcemap: false,
+                compress: {
+                    sequences: true,
+                    dead_code: true,
+                    conditionals: true,
+                    booleans: true,
+                    unused: true,
+                    if_return: true,
+                    join_vars: true,
+                    drop_console: true
+                },
+                mangle: {
+                    except: ['$super', '$', 'exports', 'require']
+                },
+                output: {
+                    comments: false
+                }
+            })
+        ],
 
     watch: isDebug,
 
@@ -73,8 +86,8 @@ module.exports = {
 
         ]
     },
-    postcss: [ 
-        autoprefixer({ browsers: ['last 3 versions'] }) 
+    postcss: [
+        autoprefixer({ browsers: ['last 3 versions'] })
     ],
     resolve: {
         extensions: [
